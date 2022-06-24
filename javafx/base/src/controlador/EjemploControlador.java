44a69947e45b343bb589319ac2175c9116557f08
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-24 17:02:17 +0200

    base javafx

---
+++ javafx/base/src/controlador/EjemploControlador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fernando
+ */
+public class EjemploControlador implements Initializable {
+
+    @FXML
+    private Button btnBoton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void click(ActionEvent event) {
+        System.out.println("Hola mundo");
+    }
+    
+}
